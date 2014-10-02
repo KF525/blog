aa7c4aa8bbab24e6e98896b77463151a61ad5892
Kate Fulton <katesuzannefulton@gmail.com>
2014-10-02 13:55:08 -0700

    Fun with jQuery

--- public/js/blog.js
+++ public/js/blog.js
@@ -1,3 +1,7 @@
+//first line says that when document is finished loading,
+//then evaluate our javascript
+//all the functions are wrapped inside this initial function
+
 $(document).ready(function() {
 
   $(".table-toggle").click(function () {
@@ -13,4 +17,13 @@ $(document).ready(function() {
   $(".laptop_toggle").click(function() {
     $(this).find('img').toggle();
   });
+
+  $(document).keypress(function(event) {
+
+     var keycode = (event.keyCode ? event.keyCode : event.which);
+
+     if(keycode == 38, 38, 40, 40, 37, 39, 37, 39, 65, 66){
+       alert("Hello");
+     }
+   });
 });
